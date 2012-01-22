e524a5b954218a30b26c8a5e48231830308e7679
Suvarshi Bhadra <sbhadra@mobiquityinc.com>
2012-01-22 15:08:59 -0500

    Initial Commit

---
+++ snapupTests/snapupTests.h
@@ -0,0 +1,13 @@
+//
+//  snapupTests.h
+//  snapupTests
+//
+//  Created by Suvarshi Bhadra on 1/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface snapupTests : SenTestCase
+
+@end
---
+++ snapupTests/snapupTests.m
@@ -0,0 +1,32 @@
+//
+//  snapupTests.m
+//  snapupTests
+//
+//  Created by Suvarshi Bhadra on 1/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "snapupTests.h"
+
+@implementation snapupTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in snapupTests");
+}
+
+@end
